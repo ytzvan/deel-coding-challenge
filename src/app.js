d621674c4649781ba4368530321f8bd030dbc42d
Ytzvan <mastino14@gmail.com>
2022-11-23 13:37:03 -0500

    Endpoint /contracts now return non-terminated contracts

--- src/app.js
+++ src/app.js
@@ -25,4 +25,22 @@ app.get('/contracts/:id', getProfile, async (req, res) =>{
     if (!authorizedUsers.includes(userId)) return res.send(401, 'Not Authorized');
     res.json(contract)
 })
+
+app.get('/contracts', getProfile, async(req, res) => {
+    const { Contract } = req.app.get('models')
+    const { profile } = req;
+    const userId = profile.dataValues.id;
+    const userType = profile.dataValues.type;
+    let query = { status: ['new', 'in_progress'] };
+    if (userType === 'contractor') {
+        query = { ...query, ContractorId: userId}
+    }
+    if (userType === 'client') {
+        query = { ...query, ClientId: userId }
+    }
+    const contracts = await Contract.findAll({
+        where: query
+      });
+    return res.json(contracts)
+});
 module.exports = app;
